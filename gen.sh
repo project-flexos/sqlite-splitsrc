fdb7258b9691623f27503db05404fc735cf48b3d
Hugo Lefeuvre <hugo.lefeuvre@manchester.ac.uk>
2021-07-26 21:04:05 +0200

    wrap everything in $NAME
    Signed-off-by: Hugo Lefeuvre <hugo.lefeuvre@manchester.ac.uk>

--- gen.sh
+++ gen.sh
@@ -11,6 +11,10 @@ sed -i -e "s/32768/${MAXSIZE}/g" tool/split-sqlite3c.tcl
 make .target_source
 tclsh ./tool/mksqlite3c.tcl
 tclsh ./tool/split-sqlite3c.tcl
-zip -r ../generated/${NAME}.zip sqlite*.c shell.* *.h
+mkdir $NAME
+mv sqlite*.c ${NAME}
+mv shell.* ${NAME}
+mv *.h ${NAME}
+zip -r ../generated/${NAME}.zip ${NAME}
 popd
 rm -rf sqlite
